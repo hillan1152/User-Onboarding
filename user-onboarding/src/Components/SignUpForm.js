ff7953e07093cc0d73b5155c658e19f9c35858ca
Anthony Hill <hillan1152@gmail.com>
2019-09-19 17:38:16 -0400

    Almost done setting up useEffect

--- user-onboarding/src/Components/SignUpForm.js
+++ user-onboarding/src/Components/SignUpForm.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { withForMik, Form, Field} from "formik";
+import { withFormik, Form, Field} from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
 
 const SignUpForm = ({values, errors, touched, status}) => {
+    
+    const [people, setPeople] = useState([]);
+    useEffect( () => {
+        if(status) {
+            setPeople([...people, status])
+        }
+    }, [status])
     return (
         <div className = "signup-form">
             <Form>
@@ -37,4 +44,26 @@ const SignUpForm = ({values, errors, touched, status}) => {
     )
 }
 
-export default SignUpForm;
+const FormikSignUpForm = withFormik({
+    mapPropsToValues({ name, email, password, termsOfService}) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            termsOfService: termsOfService || false
+        };
+    },
+    validationschema: Yup.object().shape({
+        name: Yup.string().required("Add Your Name"),
+        email: Yup.string().required("Let us know where we can contact you.")
+    }),
+    handleSubmit(values, { setStatus }) {
+        axios.post("https://reqres.in/api/users", values)
+        .then(res => {
+            setStatus(res.data);
+        })
+        .catch(err => console.log("ERROR", err.res))
+    }
+})(SignUpForm);
+console.log("This is the Formik", FormikSignUpForm);
+export default FormikSignUpForm;
